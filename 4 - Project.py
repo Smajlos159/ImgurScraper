6c61774d162855384b2168f93f82e4590e99c95d
Smajlos159 <54553829+Smajlos159@users.noreply.github.com>
2019-08-27 00:53:11 +0200

    ImgurScraper - 2.1

--- 4 - Project.py
+++ 4 - Project.py
@@ -1,6 +1,7 @@
-import requests, os, random, string, time
+import requests, os, random, string, time, ctypes
 
-html_file_prefix = """<!DOCTYPE html>
+ctypes.windll.kernel32.SetConsoleTitleW("ImgurScraper") # Console Title
+html_file_prefix = """<!DOCTYPE html> 
 <head>
     <meta charset="UTF-8">
     <title>Scraped Images</title>
@@ -24,12 +25,11 @@ h1 {
 <body bgcolor="lightblue">
 <h1 align="center">Scrapped Images</h1>
 """
-if not os.path.isfile("images.html"):
-    html_file = open("images.html", 'a+')
-    html_file.write(html_file_prefix)
-    html_file.close()
+if not os.path.isfile("images.html"):           # Creates HTML file if not already created
+    with open("images.html", 'a+') as html_file:
+        html_file.write(html_file_prefix)
 html_file = open("images.html", 'a')
-if not os.path.isdir("Images"):
+if not os.path.isdir("Images"):                 # Creates Images dir if not already created
     os.mkdir("Images")
 output_file_name = 0
 while True:
@@ -41,7 +41,7 @@ while True:
 start_time = time.time() # Starting point for timer
 while output_file_name != number_images:
     image_code = ""
-    for char in range(7):
+    for char in range(7):                        # Generates image name
         char = random.choice(string.ascii_letters + string.digits)
         image_code += char
     r = requests.get(f"https://i.imgur.com/{image_code}.png", stream=True)
@@ -51,7 +51,9 @@ while output_file_name != number_images:
         output_file_name += 1
         print(f"> Hit! [{output_file_name}/{number_images}] [URL: i.imgur.com/{image_code}.png]")
         open(f"Images\\{output_file_name}.png", 'wb').write(r.content)
-        html_file.write(f'<img src="Images\\{output_file_name}.png"></img>\n')
+        with open("images.html", 'a+') as html_file: # Writes image into the HTML file
+            html_file.write(f'<img src="Images\\{output_file_name}.png"></img>\n')
+        ctypes.windll.kernel32.SetConsoleTitleW(f"ImgurScraper [{output_file_name}/{number_images}]") # Updates console title
 
 finish_time = time.time() # Stopping point for timer
 print(f"> Done in {round(finish_time - start_time, 2)} seconds!")
