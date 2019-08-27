dcc757948840e64abdf92195f58db00770ee34ed
Smajlos159 <54553829+Smajlos159@users.noreply.github.com>
2019-08-27 14:54:18 +0200

    ImgurScraper - 2.3 - FINAL

--- ImgurScraper.py
+++ ImgurScraper.py
@@ -1,33 +1,6 @@
 import requests, os, random, string, time, ctypes
 
 ctypes.windll.kernel32.SetConsoleTitleW("ImgurScraper") # Console Title
-html_file_prefix = """<!DOCTYPE html> 
-<head>
-    <meta charset="UTF-8">
-    <title>Scraped Images</title>
-</head>
-<style>
-img {
-    margin-top: 50px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    border: 3px solid black;
-    width:700px
-}
-h1 {
-    background-color: cyan;
-    border: 3px solid black;
-    font-family: Impact;
-    font-weight: bold
-}
-</style>
-<body bgcolor="lightblue">
-<h1 align="center">Scrapped Images</h1>
-"""
-if not os.path.isfile("images.html"):           # Creates HTML file if not already created
-    with open("images.html", 'a+') as html_file:
-        html_file.write(html_file_prefix)
 html_file = open("images.html", 'a')
 if not os.path.isdir("Images"):                 # Creates Images dir if not already created
     os.mkdir("Images")
@@ -62,8 +35,6 @@ while output_file_name != number_images:
         output_file_name += 1
         print(f"> Hit! [{output_file_name}/{number_images}] [URL: i.imgur.com/{image_code}.png]")
         open(f"Images\\{output_file_name}.png", 'wb').write(r.content)
-        with open("images.html", 'a+') as html_file: # Writes image into the HTML file
-            html_file.write(f'<img src="Images\\{output_file_name}.png"></img>\n')
         current_time = time.time()  # Gets current time for CPM
         if fails != 0:
             ctypes.windll.kernel32.SetConsoleTitleW(f"ImgurScraper [Progress: {output_file_name}/{number_images}] [Hitrate: {round(output_file_name / fails * 100, 4)}%] [CPM: {int((output_file_name + fails) / (current_time - start_time) * 60)}]") # Updates console title
